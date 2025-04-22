a97fb0b293c9484d53c66ffcee8030d334e97170
kojesung <js95112345@gmail.com>
2025-04-23 01:36:02 +0900

    feat : modal 기능 추가[2주차 과제]

--- week2/week2-hw/index.js
+++ week2/week2-hw/index.js
@@ -1,5 +1,11 @@
 import { todos } from './DEFAULTTODO.js';
 
 const DEFAULT_TODO = todos;
+const modal = document.querySelector('.modal-section'); // modal 요소 가져오기
+const addBtn = document.querySelector('.input-section button'); // Todo 추가 버튼
 
 localStorage.setItem('todoList', JSON.stringify(DEFAULT_TODO));
+
+addBtn.addEventListener('click', () => {
+    modal.classList.add('show');
+});
