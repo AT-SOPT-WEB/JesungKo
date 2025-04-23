a52976e38ba8a924fa787d7da488541067789c02
kojesung <js95112345@gmail.com>
2025-04-23 14:05:29 +0900

    feat : 테이블 렌더링[2주차 과제]

--- week2/week2-hw/index.js
+++ week2/week2-hw/index.js
@@ -1,10 +1,26 @@
 import { todos } from './DEFAULTTODO.js';
 
-const DEFAULT_TODO = todos;
+const DEFAULT = todos; // 기본으로 주어진 local storage 내용
 const modal = document.querySelector('.modal-section'); // modal 요소 가져오기
+const input = document.querySelector('.input-section input'); // 할 일 입력하는 input
 const addBtn = document.querySelector('.input-section button'); // Todo 추가 버튼
+const table = document.querySelector('.table-section table'); // 할 일 table 가져오기
 
-localStorage.setItem('todoList', JSON.stringify(DEFAULT_TODO));
+localStorage.setItem('todoList', JSON.stringify(DEFAULT)); // 값 넣어두고 시작
+const myTodoList = JSON.parse(localStorage.getItem('todoList')); // 내부적으로 사용할 변수에 담기
+
+// input 핸들러
+input.addEventListener('input', () => {
+    console.log(input.value);
+});
+
+console.log(myTodoList);
+
+myTodoList.forEach((todo) => {
+    const tr = document.createElement('tr');
+    tr.innerHTML = `<th>${todo.completed}</th><th>${todo.priority}</th><th>${todo.completed}</th><th>${todo.title}</th>`;
+    table.appendChild(tr);
+});
 
 addBtn.addEventListener('click', () => {
     modal.classList.add('show');
