5b31b50783ccd7279ae67460437a47a064730ffa
kojesung <js95112345@gmail.com>
2025-05-05 02:27:48 +0900

    feat : 회원가입 레이아웃 및 stage 기반 렌더링[4주차 과제]

--- week4/week4-hw/src/pages/LoginPage.tsx
+++ week4/week4-hw/src/pages/LoginPage.tsx
@@ -1,7 +1,12 @@
+import Login from '../components/Login/Login';
 import MainContainer from '../components/MainContainer/MainContainer';
 
 const LoginPage = () => {
-    return <MainContainer>d</MainContainer>;
+    return (
+        <MainContainer>
+            <Login />
+        </MainContainer>
+    );
 };
 
 export default LoginPage;
