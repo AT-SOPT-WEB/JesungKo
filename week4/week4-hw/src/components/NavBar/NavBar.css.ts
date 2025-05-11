0f95678332ddd42e72850912dee41f1b4d13942f
kojesung <js95112345@gmail.com>
2025-05-12 01:49:10 +0900

    feat : navBar 기능 추가(라우팅, 로그아웃)[4주차 과제]

--- week4/week4-hw/src/components/NavBar/NavBar.css.ts
+++ week4/week4-hw/src/components/NavBar/NavBar.css.ts
@@ -2,10 +2,21 @@ import { style } from '@vanilla-extract/css';
 
 export const navBar = style({
     height: '110px',
-    width: 'calc(100vw - 40px)',
+    width: 'calc(100vw - 100px)',
     backgroundColor: 'lightgreen',
-    padding: '20px',
+    padding: '10px 40px 10px 40px',
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
+});
+
+export const menuSection = style({
+    display: 'flex',
+    gap: '20px',
+});
+
+export const menuButton = style({
+    background: 'none',
+    border: 'none',
+    fontSize: '20px',
 });
--- week4/week4-hw/src/components/NavBar/NavBar.tsx
+++ week4/week4-hw/src/components/NavBar/NavBar.tsx
@@ -1,16 +1,32 @@
 import { useNavigate } from 'react-router-dom';
-import { navBar } from './NavBar.css';
+import { menuButton, menuSection, navBar } from './NavBar.css';
 
 const NavBar = () => {
     const userName = localStorage.getItem('userName');
     const navigate = useNavigate();
+
+    const handleClickLogout = () => {
+        localStorage.removeItem('userName');
+        navigate('/');
+    };
+
     if (!userName) {
-        alert('잘못된 접근임돠');
-        navigate(-1);
+        alert('잘못된 접근임돠, 로그인 후에 오세요!!');
+        navigate('/');
     }
     return (
         <nav className={navBar}>
-            <section>메뉴들</section>
+            <section className={menuSection}>
+                <button onClick={() => navigate('/mypage/info')} className={menuButton}>
+                    내 정보
+                </button>
+                <button onClick={() => navigate('/mypage/search')} className={menuButton}>
+                    SOPT 회원 조회하기
+                </button>
+                <button onClick={() => handleClickLogout()} className={menuButton}>
+                    로그아웃
+                </button>
+            </section>
             <article>반갑습니다 {userName}</article>
         </nav>
     );
